241158bb7f9644256f7b2170811f0d01cd4e1f15
hank <hank.wang@dokec.com>
2012-06-14 18:25:41 +0800

    add new file

---
+++ process.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * @author Paul Bukowski <pbukowski@telaxus.com>
+ * @copyright Copyright &copy; 2006, Telaxus LLC
+ * @license MIT
+ * @version 1.0
+ * @package Srm-base
+ */
+ob_start();
+header("Content-type: text/javascript");
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT"); // date in the past
+
+
+if(!isset($_POST['url']) || !isset($_SERVER['HTTP_X_CLIENT_ID']))
+	die('alert(\'Invalid request\');');
+
+
+define('JS_OUTPUT',1);
+define('Srm_PROCESS',1);
+require_once('include.php');
+
+if(!isset($_SESSION['num_of_clients'])) {
+	Srm::alert('Session expired, restarting Srm');
+	Srm::redirect();
+	Srm::send_output();
+	define('SESSION_EXPIRED',1);
+	//session_commit();
+	//DBSession::destroy(session_id());
+} else {
+	ob_start(array('ErrorHandler','handle_fatal'));
+	Srm::process($_POST['url'],isset($_POST['history'])?$_POST['history']:false);
+	ob_end_flush();
+}
+$content = ob_get_contents();
+ob_end_clean();
+
+require_once('libs/minify/HTTP/Encoder.php');
+$he = new HTTP_Encoder(array('content' => $content));
+if (MINIFY_ENCODE)
+	$he->encode();
+$he->sendAll();
+?>
